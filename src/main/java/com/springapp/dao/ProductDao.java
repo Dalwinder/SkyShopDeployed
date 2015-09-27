5e1172e754648f326af611f6fae11ce82549905f
Bagdi, Dalwinder (Associate Software Developer) <dalli_b@icloud.com>
2015-09-27 14:54:05 +0100

    Adding Dao, Service and Model structure

---
+++ src/main/java/com/springapp/dao/ProductDao.java
@@ -0,0 +1,101 @@
+package com.springapp.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+import com.springapp.model.Product;
+
+public class ProductDao implements DaoInterface<Product, String> {
+
+	private Session currentSession;
+	
+	private Transaction currentTransaction;
+
+	public ProductDao() {
+	}
+
+	public Session openCurrentSession() {
+		currentSession = getSessionFactory().openSession();
+		return currentSession;
+	}
+
+	public Session openCurrentSessionwithTransaction() {
+		currentSession = getSessionFactory().openSession();
+		currentTransaction = currentSession.beginTransaction();
+		return currentSession;
+	}
+	
+	public void closeCurrentSession() {
+		currentSession.close();
+	}
+	
+	public void closeCurrentSessionwithTransaction() {
+		currentTransaction.commit();
+		currentSession.close();
+	}
+	
+	private static SessionFactory getSessionFactory() {
+		Configuration configuration = new Configuration().configure();
+		StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
+				.applySettings(configuration.getProperties());
+		SessionFactory sessionFactory = configuration.buildSessionFactory(builder.build());
+		return sessionFactory;
+	}
+
+	public Session getCurrentSession() {
+		return currentSession;
+	}
+
+	public void setCurrentSession(Session currentSession) {
+		this.currentSession = currentSession;
+	}
+
+	public Transaction getCurrentTransaction() {
+		return currentTransaction;
+	}
+
+	public void setCurrentTransaction(Transaction currentTransaction) {
+		this.currentTransaction = currentTransaction;
+	}
+
+	public void persist(Product entity) {
+		getCurrentSession().save(entity);
+	}
+
+	public void update(Product entity) {
+		getCurrentSession().update(entity);
+	}
+
+	public Product findById(int id) {
+		Product product = (Product) getCurrentSession().get(Product.class, id);
+		return product; 
+	}
+
+	//TODO
+	public Product findById(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void delete(Product entity) {
+		getCurrentSession().delete(entity);
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Product> findAll() {
+		List<Product> products = (List<Product>) getCurrentSession().createQuery("from Products").list();
+		return products;
+	}
+
+	public void deleteAll() {
+		List<Product> entityList = findAll();
+		for (Product entity : entityList) {
+			delete(entity);
+		}
+	}
+}
---
+++ src/main/java/com/springapp/model/Customer.java
@@ -0,0 +1,13 @@
+package com.springapp.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "customers")
+public class Customer {
+
+	
+}
