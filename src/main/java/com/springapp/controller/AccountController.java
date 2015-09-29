9b7f20f1a7fa266b9668d3cb0cbd78b6c5d4d98d
Bagdi, Dalwinder (Associate Software Developer) <dalli_b@icloud.com>
2015-09-29 12:00:11 +0100

    adding login and account controller

---
+++ src/main/java/com/springapp/controller/AccountController.java
@@ -0,0 +1,56 @@
+package com.springapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+import static spark.Spark.*;
+import static spark.Spark.get;
+
+import com.heroku.sdk.jdbc.DatabaseUrl;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/")
+public class AccountController {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String printWelcome() {
+
+		port(Integer.valueOf(System.getenv("PORT")));
+		staticFileLocation("/public");
+
+		Connection connection = null;
+		Map<String, Object> attributes = new HashMap<String, Object>();
+		try {
+			connection = DatabaseUrl.extract().getConnection();
+
+			Statement stmt = connection.createStatement();
+			stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+			stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+			ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+
+			ArrayList<String> output = new ArrayList<String>();
+			while (rs.next()) {
+				output.add( "Read from DB: " + rs.getTimestamp("tick"));
+			}
+
+			attributes.put("results", output);
+			return "index";
+		} catch (Exception e) {
+			attributes.put("message", "There was an error: " + e);
+			return "error";
+		} finally {
+			if (connection != null) try{connection.close();} catch(SQLException e){}
+		}
+	}
+
+}
