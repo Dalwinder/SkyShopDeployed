1342ce38517d4c10a3efe1ab7300c91bec18b3cc
Bagdi, Dalwinder (Associate Software Developer) <dalli_b@icloud.com>
2015-09-30 11:19:51 +0100

    fixing database connection

--- src/main/java/com/springapp/dao/ProductCatalogueDaoImp.java
+++ src/main/java/com/springapp/dao/ProductCatalogueDaoImp.java
@@ -16,43 +16,23 @@ import java.util.logging.SimpleFormatter;
 
 public class ProductCatalogueDaoImp implements ProductCatalogueDao {
 
-	private DataFieldMaxValueIncrementer productIncrementer;
-
-	public void setProductIncrementer(DataFieldMaxValueIncrementer productIncrementer){
-		this.productIncrementer = productIncrementer;
-	}
-
 	public Product insertProduct(Product product){
 		Connection connection = null;
 		try {
-			//Integer id = productIncrementer.nextIntValue();
-			//product.setId(id);
 			connection = DatabaseUrl.extract().getConnection();
-
 			Statement stmt = connection.createStatement();
-			String query = "DELETE FROM Products WHERE ID = 13";
-
-//			String query = "INSERT INTO Products (id, tid, code, name, description, price, imageUrl)"
-//					+ "VALUES (" + id + ", "
-//					+ product.getProductTypeId() + ", '"
-//					+ product.getProductCode() + "', '"
-//					+ product.getName() + "', '"
-//					+ product.getDescription() + "', "
-//					+ product.getPrice()  + ", '"
-//					+ product.getImageUrl()  + "')";
+			String query = "INSERT INTO Products (tid, code, name, description, price, imageUrl)"
+					+ "VALUES ("
+					+ product.getProductTypeId() + ", '"
+					+ product.getProductCode() + "', '"
+					+ product.getName() + "', '"
+					+ product.getDescription() + "', "
+					+ product.getPrice()  + ", '"
+					+ product.getImageUrl()  + "')";
 
 			stmt.executeQuery(query);
-		} catch (Exception e) {
-			BigDecimal test = new BigDecimal(5.99);
-			product = new Product(
-				50,
-				12,
-				e.getMessage(),
-				"error",
-				"error",
-				test,
-				"www.google.co.uk"
-			);
+		} catch (Exception e){
+
 		}
 		finally {
 			if (connection != null) try{connection.close();} catch(SQLException e){}
@@ -62,36 +42,39 @@ public class ProductCatalogueDaoImp implements ProductCatalogueDao {
 	}
 
 	public void updateProduct(Product product){
+		Connection connection = null;
+		try {
+			connection = DatabaseUrl.extract().getConnection();
+			Statement stmt = connection.createStatement();
+			String query = "UPDATE Products SET (id, tid, code, name, description, price, imageUrl)"
+					+ "tid = " + product.getProductTypeId() + ", "
+					+ "code ='" +product.getProductCode() + "', "
+					+ "name = '" + product.getName() + "', "
+					+ "description = '" + product.getDescription() + "', "
+					+ "price = " + product.getPrice()  + ", "
+					+ "imageurl ='" + product.getImageUrl()  + "'"
+					+ "where id = " + product.getId();
 
-//		Connection connection = null;
-//		try {
-//			Statement stmt = connection.createStatement();
-//			ResultSet rs = stmt.executeQuery("DELETE FROM product WHERE ID = 1");
-//			product = new Product(
-//					rs.getInt("id"),
-//					rs.getInt("tid"),
-//					rs.getString("code"),
-//					rs.getString("name"),
-//					rs.getString("description"),
-//					rs.getBigDecimal("price"),
-//					rs.getString("imageUrl")
-//			);
-//		} catch (Exception e) {
-//			attributes.put("message", "There was an error: " + e);
-//		} finally {
-//			if (connection != null) try{connection.close();} catch(SQLException e){}
-//		}
+			stmt.executeQuery(query);
+		} catch (Exception e){
+
+		}
+		finally {
+			if (connection != null) try{connection.close();} catch(SQLException e){}
+		}
 	}
 
 	public void deleteProduct(Product product){
 		Connection connection = null;
 		try {
+			connection = DatabaseUrl.extract().getConnection();
 			Statement stmt = connection.createStatement();
-			ResultSet rs = stmt.executeQuery("DELETE FROM product WHERE ID = 1");
+			String query = "DELETE FROM Products WHERE id = " + product.getId();
+			stmt.executeQuery(query);
+		} catch (Exception e){
 
-		} catch (Exception e) {
-			System.out.println("There was an error: " + e);
-		} finally {
+		}
+		finally {
 			if (connection != null) try{connection.close();} catch(SQLException e){}
 		}
 	}
